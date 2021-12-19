d8736a1a7e9fa7564a5bdc2b4b5c3af6b240dcd8
egonlr <egonlr@hotmail.com>
2021-12-19 07:59:32 -0300

    Trabalhando com arquivo fonte do layout

--- app/src/main/java/com/example/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/com/example/agenda/ui/activity/ListaAlunosActivity.java
@@ -1,26 +1,29 @@
-package com.example.agenda;
+package com.example.agenda.ui.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.agenda.R;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         TextView aluno = new TextView(this);
-        setContentView(R.layout.activity_main_);
+        setContentView(R.layout.activity_lista_alunos);
+        setTitle("Lista de alunos");
         List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "José", "Egon", "Pedro", "Maria"));
-        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
         listaDeAlunos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos));
 
     }
